5fbbf60981b5d86a0e5a51cc102b169ed68bd83b
guitarbuju <gerardoamadrid@gmail.com>
2024-01-30 11:37:56 +0100

    switched so portfolio is shown by default

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -7,12 +7,11 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import camera from "../../assets/icons8-camera-24.png";
 import CardESP from "../../components/Card/CardESP";
-import espana from '../../assets/icons8-spain-48.png'
-import usa from '../../assets/icons8-usa-48.png'
-
+import espana from "../../assets/icons8-spain-48.png";
+import usa from "../../assets/icons8-usa-48.png";
 
 const About = () => {
-  const [checked, setChecked] = useState(true);
+  const [checked, setChecked] = useState(false);
 
   const handleCheck = () => {
     setChecked(!checked);
@@ -68,29 +67,28 @@ const About = () => {
             stiffness: 50,
           }}
         >
-        {/* TOGGLE LANGUAGE */}
+          {/* TOGGLE LANGUAGE */}
           <div className="flex align-middle justify-center gap-2  mb-2 mr-60">
-            <img src={usa} className="w-6 opacity-80"/>
+            <img src={usa} className="w-6 opacity-80" />
             <input
               type="checkbox"
               className="toggle toggle-xs mt-2"
               onChange={handleLanguage}
             />
-            <img src={espana} className="w-8 opacity-80"/>
+            <img src={espana} className="w-8 opacity-80" />
           </div>
 
           <h1 className="text-blue-100 text-5xl">Gerardo Madrid</h1>
-            
-            <h2 className="text-blue-100 text-2xl opacity-60 mt-2">
-            {language ? 'Developer' : 'Desarrollador'}</h2>
+
+          <h2 className="text-blue-100 text-2xl opacity-60 mt-2">
+            {language ? "Developer" : "Desarrollador"}
+          </h2>
           <p className="text-blue-100  opacity-40 mt-0 text-sm">
-          {language ? 'inspiring, engaging and functional development' : 'desarrollo funcional que conecta e inspira ...'}  
+            {language
+              ? "inspiring, engaging and functional development"
+              : "desarrollo funcional que conecta e inspira ..."}
           </p>
-        
-          
-          
-         
-          
+
           <p className="text-blue-100  opacity-40 mt-0 text-xs">
             <a href="mailto:gerardoamadrid@gmail.com">
               gerardoamadrid@gmail.com
@@ -102,7 +100,9 @@ const About = () => {
                 className="flex align-middle gap-1 mt-0 text-xs italic opacity-80"
                 style={{ color: "#5be2cd" }}
               >
-                {language ? '...Take a look at my gallery....' : 'Mira mi galeria de Imagenes ...'}
+                {language
+                  ? "...Take a look at my gallery...."
+                  : "Mira mi galeria de Imagenes ..."}
                 <span className="w-10 mb-6">
                   <img src={camera} />
                 </span>
@@ -110,18 +110,16 @@ const About = () => {
             </Link>
           </div>
           <div className="flex gap-2 justify-center align-middle">
-            <div>
-              <h1 className="w-20 text-center text-xs  text-blue-100 leading-relaxed opacity-50 mt-2 mr-2">
-               {language ? 'Experience & Education': 'Experiencia y Educaciòn'} 
-              </h1>
-            </div>
+            <h1 className="w-20 text-xs text-center text-blue-100 leading-relaxed opacity-50 mt-4">
+              {language ? "Portfolio" : "Portafolio"}
+            </h1>
             <input
               type="checkbox"
               className="toggle toggle-xs mt-4"
               onChange={handleCheck}
             />
-            <h1 className="w-20 text-xs text-center text-blue-100 leading-relaxed opacity-50 mt-4">
-             {language ? 'Portfolio' : 'Portafolio'} 
+            <h1 className="w-20 text-center text-xs  text-blue-100 leading-relaxed opacity-50 mt-2 mr-2">
+              {language ? "Experience & Education" : "Experiencia y Educaciòn"}
             </h1>
           </div>
 
@@ -134,15 +132,15 @@ const About = () => {
             >
               <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
             </svg>
-            <a href='https://github.com/guitarbuju'>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="#e1e8f0"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
+            <a href="https://github.com/guitarbuju">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="#e1e8f0"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+              </svg>
             </a>
             <a href="https://www.linkedin.com/in/gerardomadriddev/">
               <svg
@@ -191,7 +189,7 @@ const About = () => {
                 className="sm:text-xs ml-40 italic opacity-60"
                 style={{ color: "#5be2cd " }}
               >
-                {randomQuote.trimAuthor }
+                {randomQuote.trimAuthor}
               </div>
             </footer>
           </blockquote>
@@ -200,11 +198,14 @@ const About = () => {
 
       <div className="mt-20 mb-10 " style={{ marginLeft: "30vw" }}>
         {language ? <Card /> : <CardESP />}
-        {checked ? <Card3 language={language} /> : <Card4 language={language}/>}
+        {checked ? (
+          <Card3 language={language} />
+        ) : (
+          <Card4 language={language} />
+        )}
       </div>
     </div>
   );
 };
 
 export default About;
-
